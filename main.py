408fdef22b1593ad66f2945e8c9cf1842a9824a6
peteCoder <petercodercoder@gmail.com>
2023-12-05 01:50:24 -0800

    redesign app

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
-# main.py
-from server.api import app  # Import the FastAPI app instance from server.api
+from fastapi import FastAPI
+
+from typing import Union, Optional
+# Pydantic
+from pydantic import BaseModel
+
+
+
+
+# Models Schema
+class Post(BaseModel):
+    id: Optional[str] = None
+    title: str
+    content: str
+    is_published: bool = False
+    rating: Union[int, float] = None
+
+
+# Initialize app
+app = FastAPI()
+
+
+MY_POSTS = {
+    "data": []
+}
+
+
+# Define path operator and function
+@app.get("/")
+async def hello():
+    return {"Post API": "Welcome to my API"}
 
--- server/api.py
+++
@@ -1,7 +0,0 @@
-from server.routes import router as ScoresAPIRouter
-from fastapi import FastAPI
-
-app = FastAPI()
-
-app.include_router(ScoresAPIRouter)
-
