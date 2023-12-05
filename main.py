dd47be2a85df00fb82a8626c8ca66bf5821aa307
peteCoder <petercodercoder@gmail.com>
2023-12-05 01:34:18 -0800

    provided the handler of app instance

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import uvicorn
 from dotenv import dotenv_values
+from server.api import app
 
 config = dotenv_values(".env")
 
 
 if __name__ == "__main__":
-  uvicorn.run("server.api:app", host="127.0.0.1", port=8000, reload=True)
+  uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
 
 
