d67be9d7ef8bac2093d132c5c5067ee52d1dead1
peteCoder <petercodercoder@gmail.com>
2023-12-05 01:13:58 -0800

    changed main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
-PORT = int(config["PORT"])
-
 
 if __name__ == "__main__":
   uvicorn.run("server.api:app", host="127.0.0.1", port=8000, reload=True)
